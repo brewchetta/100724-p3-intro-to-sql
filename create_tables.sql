b2e6d2645e807bea732b25907dd6fcdad77aa8e8
brewchetta <chett.tiller@gmail.com>
2024-07-18 12:02:19 -0400

    starting companies is easy

--- create_tables.sql
+++ create_tables.sql
@@ -2,3 +2,17 @@
 
 -- IN ORDER TO UTILIZE THE FILE, RUN THIS COMMAND (minus the --)
     -- sqlite3 main.db < create_tables.sql
+
+CREATE TABLE employees (
+    id INTEGER PRIMARY KEY,
+    first_name TEXT,
+    last_name TEXT,
+    title TEXT,
+    age NUMBER,
+    salary NUMBER
+);
+
+-- CREATE - INSERT
+-- READ - SELECT (WHERE)
+-- UPDATE - UPDATE SET (WHERE)
+-- DELETE - DELETE FROM
