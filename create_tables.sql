5586686b9357e82fa7c061075856af290fd7538f
brewchetta <chett.tiller@gmail.com>
2024-11-21 11:55:53 -0500

    I DIDNT CLEAR THE OUTPUT ONCEgit statusgit status!

--- create_tables.sql
+++ create_tables.sql
@@ -3,7 +3,14 @@
 -- IN ORDER TO UTILIZE THE FILE, RUN THIS COMMAND (minus the --)
     -- sqlite3 main.db < create_tables.sql
 
-CREATE TABLE ???? (
+CREATE TABLE IF NOT EXISTS surf_data (
+id INTEGER PRIMARY KEY,
+date TEXT NOT NULL,
+wave_height INTEGER DEFAULT 3,
+wind_strength INTEGER,
+tide TEXT,
+water_temp INTEGER,
+wind_direction TEXT
 );
 
 -- CREATE - INSERT
